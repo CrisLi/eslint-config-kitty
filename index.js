d2ae13d0464616b13b303e78862576913375c630
Chris Li <chrisandkitty@163.com>
2017-04-25 16:14:14 +0800

    release some rules

--- index.js
+++ index.js
@@ -5,6 +5,13 @@ module.exports = {
     'func-names': ['error', 'never'],
     'space-before-function-paren': ['error', 'never'],
     'comma-dangle': 0,
-    'dot-notation': 0
+    'dot-notation': 0,
+    'newline-per-chained-call': ['error', { ignoreChainWithDepth: 6 }],
+    'import/no-extraneous-dependencies': 0,
+    'arrow-body-style': [1, 'as-needed'],
+    'no-unused-vars': [1],
+    'jsx-a11y/label-has-for': [0],
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+    'react/prop-types': [0]
   }
 };
