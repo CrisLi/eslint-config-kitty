8e3b05093a236ca5426fd16899ddb3dc74429d2a
Chris Li <chrisandkitty@163.com>
2017-05-08 11:52:07 +0800

    Relax some rules

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 module.exports = {
   extends: 'airbnb',
   rules: {
-    'no-console': [0],
+    'no-console': ['off'],
+    quotes: ['warn'],
+    'no-alert': ['warn'],
     'func-names': ['error', 'never'],
     'space-before-function-paren': ['error', 'never'],
-    'comma-dangle': 0,
-    'dot-notation': 0,
+    'comma-dangle': 'off',
+    'dot-notation': 'off',
     'newline-per-chained-call': ['error', { ignoreChainWithDepth: 6 }],
-    'import/no-extraneous-dependencies': 0,
-    'arrow-body-style': [1, 'as-needed'],
-    'no-unused-vars': [1],
-    'jsx-a11y/label-has-for': [0],
-    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
-    'react/prop-types': [0]
+    'import/no-extraneous-dependencies': 'off',
+    'arrow-body-style': ['warn', 'as-needed'],
+    'prefer-arrow-callback': ['warn'],
+    'no-unused-vars': ['warn'],
+    'jsx-a11y/label-has-for': ['off'],
+    'react/jsx-filename-extension': ['warn', { extensions: ['.js', '.jsx'] }],
+    'react/prop-types': ['off']
   }
 };
